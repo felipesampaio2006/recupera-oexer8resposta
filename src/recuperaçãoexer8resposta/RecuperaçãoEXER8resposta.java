db7f13eed63f05772a9acf625651ce85443de86f
felipe Sampaio <samlipe2006@gmail.com>
2021-07-15 16:11:59 -0300

    recuperação feito 15/07/2021

---
+++ src/recuperaçãoexer8resposta/RecuperaçãoEXER8resposta.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recuperaçãoexer8resposta;
+
+/**
+ *
+ * @author Daniela
+ */
+public class RecuperaçãoEXER8resposta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       for(int cont=1;cont<=15;cont+=2){
+            System.out.println(cont);
+    }
+    }
+}
